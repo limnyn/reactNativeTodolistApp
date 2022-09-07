9be41d4f47884d9a9a2cb5d868fdd232e69b1d40
limnyn <hogandi001@gmail.com>
2022-09-07 15:23:03 +0900

    challenge 1 fin

--- App.js
+++ App.js
@@ -1,20 +1,110 @@
+/*
+  코드 챌린지
+
+    1. 마지막으로 시작한 곳 기억하기( travel, work 모드를 기억하고 앱 재시작시 이어서 시작)
+    2. 삭제가 아닌 완료 버튼과 완료 효과 구현하기 [mutate 쓰지않기]
+    3. 유저가 text를 수정할 수 있게하기
+    
+    내가 어디있는지 기억하기
+    완료표시
+    완료, 수정, 삭제 버튼 및 기능 구현
+*/
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-  Pressable,
+  TextInput,
+  ScrollView,
+  Alert,
+  Platform,
 } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from "./colors";
+import { Fontisto } from "@expo/vector-icons";
+const STORAGE_KEY = "@toDos";
+const STATUS_KEY = "@isWorking";
 
 export default function App() {
   const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  useEffect(() => {
+    loadStatus();
+    loadToDos();
+  }, []);
+  useEffect(() => {
+    saveStatus(working);
+  }, [working]);
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
+  const saveStatus = async (iswork) => {
+    await AsyncStorage.setItem(STATUS_KEY, iswork.toString());
+  };
+  const loadStatus = async () => {
+    const s = await AsyncStorage.getItem(STATUS_KEY);
+    if (s) {
+      setWorking(JSON.parse(s));
+    } else {
+      setWorking(true);
+    }
+  };
+
+  const saveToDos = async (toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    if (s) {
+      setToDos(JSON.parse(s));
+    }
+  };
+
+  const addToDo = async () => {
+    if (text === "") {
+      return;
+    }
+    //save to do
+    const newToDos = {
+      ...toDos,
+      [Date.now()]: { text, working },
+    };
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
+  };
+
+  const deleteTodo = (key) => {
+    if (Platform.OS === "web") {
+      const ok = confirm("Delete To do?");
+      if (ok) {
+        const newToDos = { ...toDos };
+        delete newToDos[key];
+        setToDos(newToDos);
+        saveToDos(newToDos);
+      }
+    } else {
+      Alert.alert("Delete To Do?", "Are you sure?", [
+        { text: "Cancel" },
+        {
+          text: "I'm Sure",
+          onPress: () => {
+            const newToDos = { ...toDos };
+            delete newToDos[key];
+            setToDos(newToDos);
+            saveToDos(newToDos);
+          },
+        },
+      ]);
+    }
+    return;
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -37,10 +127,29 @@ export default function App() {
           </Text>
         </TouchableOpacity>
       </View>
+      <TextInput
+        returnKeyType="done"
+        onSubmitEditing={addToDo}
+        value={text}
+        onChangeText={onChangeText}
+        placeholder={working ? "Add a To Do" : "Where do you want to go"}
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <TouchableOpacity onPress={() => deleteTodo(key)}>
+                <Fontisto name="trash" size={20} color={theme.grey} />
+              </TouchableOpacity>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -55,5 +164,28 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
